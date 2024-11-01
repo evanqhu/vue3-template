3e7e0f75fd3bbb71877cad41b7fb7c8a0df53506
evanqhu <qianghu98@163.com>
2024-11-01 15:44:47 +0800

    chore: 🧰 update

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ Env=$1
 # 启动服务
 # 以对应用启动时，1）部分文件没有权限；2）/data/logs 挂载到容器后普通用户无写权限
 sudo chown -R ${AppUser}:${AppGroup} /data
-NODE_ENV=production node server/index.js
+NODE_ENV=production PORT=5000 node server/index.js
