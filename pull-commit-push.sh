635e479fd3610d05424256c8d2dfceac34984b66
evanqhu <qianghu98@163.com>
2024-12-03 23:09:16 +0800

    feat: 🚀 update

--- pull-commit-push.sh
+++ pull-commit-push.sh
@@ -7,13 +7,13 @@ git ls-remote &> /dev/null
 exit_status=$?
 
 if [ $exit_status -eq 0 ]; then
-    echo "远程仓库存在，执行 git pull 和 git push"
-    git pull
-    git add -A
-    git-cz
-    git push
+  echo "远程仓库存在，执行 git pull 和 git push"
+  git pull
+  git add -A
+  git-cz
+  git push
 else
-    echo "远程仓库不存在，仅执行 git add 和 git-cz"
-    git add -A
-    git-cz
+  echo "远程仓库不存在，仅执行 git add 和 git-cz"
+  git add -A
+  git-cz
 fi
